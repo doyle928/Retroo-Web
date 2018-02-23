0b5e92e08fc07430475b32d46cd43e8a28c48379
doyle928 <doyle928@yahoo.com>
2018-02-23 00:48:57 -0500

    added error messages

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-$.getJSON('https://api.ipify.org?format=json', function(data) {
-  console.log(JSON.stringify(data, null, 2));
-  $(".welcomeIp").attr("data-text", "Welcome " + data.ip);
-  $(".welcomeIp").text("welcome " + data.ip);
+$.getJSON('https://api.ipify.org?format=json', function (data) {
+    console.log(JSON.stringify(data, null, 2));
+    $(".welcomeIp").attr("data-text", "Welcome " + data.ip);
+    $(".welcomeIp").text("welcome " + data.ip);
+});
+
+
+
+$(document).ready(function () {
+
+    //var error = $(".error");
+    setTimeout(function () {
+        $(".error").fadeIn(0);
+        var elem = document.getElementById("virusBar");
+        var width = 1;
+        var id = setInterval(frame, 40);
+
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+            } else {
+                width++;
+                elem.style.width = width + '%';
+            }
+        }
+
+    }, 3000);
+
+
+    $(".error").click(function () {
+        $(".error2").fadeIn(0);
+    })
 });
